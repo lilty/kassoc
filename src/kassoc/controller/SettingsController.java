59ba86ce237b967471e4739512c93b3f4349e6f2
bastien11 <scherrer.bastien@gmail.com>
2016-06-07 11:24:21 +0200

    add setings

---
+++ src/kassoc/controller/SettingsController.java
@@ -0,0 +1,37 @@
+package kassoc.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * Class kassoc.controller.SettingsController
+ * @author Bastien
+ * @project kassoc
+ */
+public class SettingsController {
+    /**
+     * The Save.
+     */
+    public Button save;
+    /**
+     * The Cancel.
+     */
+    public Button cancel;
+    /**
+     * The Settings.
+     */
+    public Label settings;
+    /**
+     * The Change mail.
+     */
+    public TextField changeMail;
+    /**
+     * The Edit assocation.
+     */
+    public TextField editAssocation;
+    /**
+     * The Delete assocation.
+     */
+    public TextField deleteAssocation;
+}
