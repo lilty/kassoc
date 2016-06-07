44f72d02f911a67586e15b6bdbe42dc58ef80852
lilty <ylilt71@gmail.com>
2016-06-07 15:05:50 +0200

    Changes (3)

--- src/kassoc/controller/AdminController.java
+++ src/kassoc/controller/AdminController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
  * The type Admin controller.
  */
@@ -12,7 +14,7 @@ public class AdminController extends BaseController {
      * Add actuality.
      * @param e the e
      */
-    public void addActuality(ActionEvent e) {
+    public void addActuality(ActionEvent e) throws IOException {
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         stage.setTitle("Kassoc - Dashboard");
         this.gotoScene(stage, "/kassoc/view/new-actuality.fxml");
