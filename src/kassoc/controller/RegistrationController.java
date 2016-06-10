3c0b035a3af7cb0c7b4b03f8fce93fcb549ec674
bastien11 <scherrer.bastien@gmail.com>
2016-06-10 09:41:26 +0200

    add ok tab registration and form for General registration

--- src/kassoc/controller/RegistrationController.java
+++ src/kassoc/controller/RegistrationController.java
@@ -15,14 +15,50 @@ import java.io.IOException;
  * @project kassoc
  */
 public class RegistrationController extends BaseController {
-    public Accordion accordion;
-    public TitledPane alumniceRegistrationPane;
-    public TitledPane bdeRegistrationPane;
-    public TitledPane bdsRegistrationPane;
-    public TitledPane regentRegistrationPane;
+
+    public DatePicker birthdayInput;
+    public TextField businessInput;
+    public TextField levelInput;
+    public TextField mailInput;
+    public TextField lastNameInput;
+    public Button okAluInput;
+    public Button okBdeInput;
+    public TextField firstNameInput;
+    public Button okBdsInput;
     public TextField addressInput;
     public TextField numberInput;
     public Button academicRecordInput;
+    public TextField sportPracticInput;
+    public TextArea thematicInput;
+    public CheckBox tournamentInput;
+
+    public void okAction(ActionEvent e) throws IOException{
+        org.hibernate.Transaction tx = Core.getCurrentSession().beginTransaction();
+        try{
+            String name = lastNameInput.getText();
+            if(name == null || name.isEmpty()){
+                new Alert(Alert.AlertType.ERROR, "The name provaded is not valid.").show();
+                tx.rollback();
+                return;
+            }
+            String firstName = firstNameInput.getText();
+            if(firstName == null || firstName.isEmpty()){
+                new Alert(Alert.AlertType.ERROR, "The firstname provaded is not valid.").show();
+                tx.rollback();
+                return;
+            }
+            String address = addressInput.getText();
+            if(address == null || address.isEmpty()){
+                new Alert(Alert.AlertType.ERROR, "The address provaded is not valid.").show();
+                tx.rollback();
+                return;
+            }
+        }
+        catch(ConstraintViolationException t){
+            tx.rollback();
+            new Alert(Alert.AlertType.ERROR, "You are already registered").show();
+        }
+    }
 
     public void academicRecordAction(ActionEvent e) throws IOException {
         org.hibernate.Transaction tx = Core.getCurrentSession().beginTransaction();
@@ -46,6 +82,32 @@ public class RegistrationController extends BaseController {
 
 
         } catch (ConstraintViolationException t) {
+            tx.rollback();
+            new Alert(Alert.AlertType.ERROR, "You are already registered").show();
+        }
+    }
+
+    public void okAluAction(ActionEvent actionEvent) throws Throwable{
+        org.hibernate.Transaction tx = Core.getCurrentSession().beginTransaction();
+        try{
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setTitle("Kassoc - Dashboard");
+            this.gotoScene(stage, "/kassoc/view/dashboard.fxml");
+        }
+        catch(ConstraintViolationException t){
+            tx.rollback();
+            new Alert(Alert.AlertType.ERROR, "You are already registered").show();
+        }
+    }
+
+    public void okBdsInput(ActionEvent actionEvent) throws IOException{
+        org.hibernate.Transaction tx = Core.getCurrentSession().beginTransaction();
+        try{
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setTitle("Kassoc - Dashboard");
+            this.gotoScene(stage, "/kassoc/view/dashboard.fxml");
+        }
+        catch(ConstraintViolationException t){
             tx.rollback();
             new Alert(Alert.AlertType.ERROR, "You are already registered").show();
         }
